9d74f7804541668fb45e314cf1bd8bc9f908f390
LordNEWNS <80376074+LordNEWNS@users.noreply.github.com>
2023-09-06 18:27:41 -0400

    Pass mouseenter funtion to js
    Changed the colour change function from css hover to js mouseenter
    for better user customization, and add additional variables for size of
    grid and sixe of pixle as well as colour of grid and sketch to js.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const main = document.getElementById('grid');
-let length = 16;
-let width = 16;
-let gridArea = length * width;
-
+const grid = document.getElementById('grid');
+const body = document.getElementById('body')
+let heightOfGrid = 16;
+let widthOfGrid = 16;
+let gridArea = heightOfGrid * widthOfGrid;
+let gridPeiceSize = 8
+let colourOfGrid = 'grey'
+let colourOfSketch = 'black'
 
 
 function creatGameBoard() {
+    grid.style.height = (heightOfGrid * gridPeiceSize) + 'px';
+    grid.style.width = (widthOfGrid * gridPeiceSize) + 'px'; 
     for (let i = 1; i <= gridArea; i++) {
         const div = document.createElement('div');
         div.classList.add('etchGridPeice');
-        main.appendChild(div);
+        grid.appendChild(div);
     };
+
+    // change colour of grid based on selection
+    const etchGridPeice = document.getElementsByClassName('etchGridPeice');
+   for (let i = 0; i < etchGridPeice.length; i++) {
+    etchGridPeice[i].style.backgroundColor = colourOfGrid;
+    etchGridPeice[i].style.height = gridPeiceSize + 'px';
+    etchGridPeice[i].style.width = gridPeiceSize + 'px';
+    etchGridPeice[i].addEventListener('mouseenter', function(e) {
+        e.target.style.backgroundColor = colourOfSketch;
+    });
+   };
+
 };
    
 creatGameBoard()
